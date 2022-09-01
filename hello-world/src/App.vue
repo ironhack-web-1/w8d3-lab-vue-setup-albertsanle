26b11780fc605ab5a756dc1f294923dd5cbd14ca
Alberto Santamaria <alberto.santamaria.leon@gmail.com>
2022-09-01 09:40:16 +0200

    done

---
+++ hello-world/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Vue 3!</h1>
+    <about-us></about-us>
+  </div>
+</template>
+
+<script>
+import AboutUs from "./components/AboutUs.vue";
+
+export default {
+  name: "App",
+  components: { AboutUs },
+};
+</script>
+
+<style></style>
---
+++ hello-world/src/components/AboutUs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>My first Vue project</h1>
+    <p>{{ aboutMe }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      aboutMe:
+        "Lorem ipsum dolor sit amet consectetur, adipiscing elit conubia dignissim morbi, dis fusce volutpat velit. Nunc cubilia quis iaculis diam tempor sed morbi sociosqu malesuada facilisi, aenean auctor mus habitasse etiam leo magnis ridiculus. Tempor eu conubia nunc lectus et lacus vestibulum congue quisque nisi, aliquet nascetur litora tempus ullamcorper magnis iaculis nulla imperdiet hac, natoque ut maecenas accumsan dictum commodo urna lacinia fusce.",
+    };
+  },
+};
+</script>
